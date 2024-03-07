7613731178bde68b4da0823c42d7704c66b8e640
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-07 21:48:35 +0700

    first commit

---
+++ src/main/java/iegcode/Javaspringlogging/JavaSpringLoggingApplication.java
@@ -0,0 +1,13 @@
+package iegcode.Javaspringlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iegcode/Javaspringlogging/JavaSpringLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package iegcode.Javaspringlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
